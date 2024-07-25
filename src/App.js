36b1acd0870227df3c92b7cd7622657012c5a210
Bhupendra Gahlot <82636246+bhupendragahlot@users.noreply.github.com>
2024-07-25 12:30:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./app.css";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <div class="menu">
+          <div class="logo">
+            <a href="/">BG</a>
+          </div>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Services</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+            <li>
+              <a href="/">Feedback</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div class="img"></div>
+      <div class="center">
+        <div class="title">Welcome My Website</div>
+        <div class="sub_title">Create HTML & CSS React.js </div>
+        <div class="btns">
+          <button>Learn More</button>
+          <button>Subscribe</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
